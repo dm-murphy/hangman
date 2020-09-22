dc3b1d8ef977a3e50621998c9b7588f93a0024c4
Daniel Murphy <danielmichaelmurphy@gmail.com>
2020-09-21 21:38:35 -0400

    Added mock functions for saving game state and added initial player guess functions. Next step is checking guess against the secret word.

--- lib/round.rb
+++ lib/round.rb
@@ -1,3 +1,5 @@
+# Next step is check guess
+
 # frozen_string_literal: true
 
 class Round
@@ -5,13 +7,49 @@ class Round
   def initialize
     @current_dictionary = []
     @secret_word = []
-    @strikes = 5
+    @strikes = 0
+    @incorrect_letters = []
+    @current_guess = ''
   end
 
   def start_round
     load_dictionary
     generate_secret_word(@current_dictionary)
+    start_player_turn
+  end
+
+  def start_player_turn
     display_round_state
+    prompt_save_game
+  end
+
+  def prompt_save_game
+    puts "Would you like to save your game? (Enter 'save' or enter any other key to skip)"
+    result = gets.chomp
+    if result.downcase == 'save' || result.downcase == "'save'"
+      save_game
+    else
+      guess_letter
+    end
+  end
+
+  def save_game
+    # Use to save round state
+    puts "Not ready yet!"
+  end
+
+  def guess_letter
+    loop do
+      puts 'Guess a letter from the word: '
+      @current_guess = gets.chomp
+      break if check_valid?
+
+      puts "That's not valid."
+    end
+  end
+
+  def check_valid?
+    @current_guess.count("a-zA-Z").positive? && @current_guess.length == 1
   end
 
   def load_dictionary
@@ -30,6 +68,7 @@ class Round
   def display_round_state
     display_blank_spaces
     display_strikes
+    display_incorrect_letters
   end
 
   def display_blank_spaces
@@ -44,5 +83,15 @@ class Round
     @strikes.times { print '* ' }
     puts
     puts 'Warning! Final strike!' if @strikes == 5
+  end
+
+  def display_incorrect_letters
+    unless @incorrect_letters.empty?
+
+    puts 'Incorrect Letters: '
+    print @incorrect_letters
+    puts
+    puts
+    end
   end
 end
