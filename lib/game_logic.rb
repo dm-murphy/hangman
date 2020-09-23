60ef3205ec18fc44a1642cfc1bb4a0b57ae2b8d4
Daniel Murphy <danielmichaelmurphy@gmail.com>
2020-09-23 16:18:41 -0400

    Added methods to check guesses.

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,1 +1,19 @@
 # frozen_string_literal: true
+
+module GameLogic
+
+  def check_guess    
+    check_strikes
+    check_blanks
+  end
+
+  def check_strikes
+    @strikes += 1 unless @secret_word.include?(@current_guess.downcase)
+  end
+
+  def check_blanks
+    @secret_word.each_with_index do |l, index|
+      @blanks[index] = l if l == @current_guess.downcase
+    end
+  end
+end
