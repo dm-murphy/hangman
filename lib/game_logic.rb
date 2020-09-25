f3b0746a226482247c990d3e0bc539151c8fcc86
Daniel Murphy <danielmichaelmurphy@gmail.com>
2020-09-24 20:15:39 -0400

    Moved #guess_letter and #guess_check_valid? from Round class to GameLogic module

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,6 +1,27 @@
 # frozen_string_literal: true
 
+# Contains logic for taking and checking guesses
 module GameLogic
+  @secret_word = []
+  @strikes = 0
+  @blanks = []
+  @incorrect_letters = []
+  @current_guess = ''
+
+  def guess_letter
+    loop do
+      puts 'Guess a letter from the word: '
+      @current_guess = gets.chomp
+      break if check_valid?
+
+      puts "That's not valid."
+    end
+  end
+
+  def check_valid?
+    @current_guess.count('a-zA-Z').positive? && @current_guess.length == 1 &&
+      @incorrect_letters.include?(@current_guess) == false
+  end
 
   def check_guess
     check_strikes
